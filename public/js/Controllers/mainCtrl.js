60a6772cd4cc0a14f4b64bbbfd193feb91587597
Connor <connorsteele604@gmail.com>
2017-10-25 12:58:53 -0700

    general code cleanup and formatting

--- public/js/Controllers/mainCtrl.js
+++ public/js/Controllers/mainCtrl.js
@@ -1,148 +1,135 @@
 angular.module('mainCtrl', [])
 
     .controller('mainController', function($scope, $http, Comment) {
-    // $scope.editing = false;
+      // object to hold all the data for the new comment form
+      $scope.commentData = {};
 
-    // Array for collecting all desired deleted comments
-    $scope.myCartItems = [];
+      // reload the data
+      $scope.loading = true;
+      // $scope.editing = false;
 
-    // Add items for batch delete into the array
-    $scope.addToCart = function(item)
-    {
-        if ($scope.myCartItems.indexOf(item) == -1) {
-            $scope.myCartItems.push(item);
-        } else {
-             $scope.myCartItems.splice(item,1);
-        }
-    }
-    // Delete from array if un-checked box
-    $scope.removeFromCart=function(item){
-        $scope.myCartItems.splice(item,1);
-    }
+      // Array for collecting batch delete items
+      $scope.myCartItems = [];
 
+      // get all the comments first and bind it to the $scope.comments object
+      Comment.get()
+          .success(function(data) {
+              $scope.comments = data;
+              $scope.loading = false;
+          });
 
-    // Batch Delete all comments in myCartItems array
-    $scope.deleteAllSelectedComments=function(array) {
-        angular.forEach(array, function(value, key) {
-              // todo code for deletion
-            $scope.loading = true;
-            Comment.destroy(value.id)
-                .success(function(data) {
-                        // if successful, we'll need to refresh the comment list
-                    Comment.get()
-                    .success(function(getData) {
-                      $scope.comments = getData;
-                      $scope.loading = false;
-                      $scope.myCartItems = [];
-                    });
-                });
-            });
+      // Add items for batch delete into the array
+      $scope.addToCart = function(item)
+      {
+          if ($scope.myCartItems.indexOf(item) == -1) {
+              $scope.myCartItems.push(item);
+          } else {
+               $scope.myCartItems.splice(item,1);
           }
-
-    // object to hold all the data for the new comment form
-    $scope.commentData = {};
-
-    // loading variable to show the spinning loading icon
-    $scope.loading = true;
-
-    // get all the comments first and bind it to the $scope.comments object
-    Comment.get()
-        .success(function(data) {
-            $scope.comments = data;
-            $scope.loading = false;
-        });
-
-    // function to handle submitting the form
-    $scope.submitComment = function() {
-        $scope.loading = true;
-        // save the comment. pass in comment data from the form
-        Comment.save($scope.commentData)
-            .success(function(data) {
-                $scope.commentData = {};
-                // if successful, we'll need to refresh the comment list
-                Comment.get()
-                    .success(function(getData) {
+      }
+      // Delete from array if un-checked box
+      $scope.removeFromCart=function(item){
+          $scope.myCartItems.splice(item,1);
+      }
+      // Batch Delete all comments in myCartItems array
+      $scope.deleteAllSelectedComments=function(array) {
+        if (confirm("Are you sure you want to delete all of these posts?")) {
+          //go over each value iin the returned array
+          angular.forEach(array, function(value, key) {
+              $scope.loading = true;
+              //destroy each item in the array
+              Comment.destroy(value.id)
+                  .success(function(data) {
+                      // if successful, we'll need to refresh the comment list
+                    Comment.get()
+                      //reset all the variables
+                      .success(function(getData) {
                         $scope.comments = getData;
                         $scope.loading = false;
-                    });
-
-            })
-            .error(function(data) {
-                console.log(data);
+                        $scope.myCartItems = [];
+                      });
+                  });
             });
-    };
-    $scope.updateSubmitForm = function(id) {
-      //Load the comments
-      $scope.loading = true;
+        };
+      }
+      // function to handle submitting the form
+      $scope.submitComment = function() {
+          $scope.loading = true;
+          // save the comment. pass in comment data from the form
+          Comment.save($scope.commentData)
+              .success(function(data) {
+                  $scope.commentData = {};
+                  // if successful, we'll need to refresh the comment list
+                  Comment.get()
+                      .success(function(getData) {
+                          $scope.comments = getData;
+                          $scope.loading = false;
+                      });
 
-      //Get the comment based on ID
-      Comment.get()
-        .success(function(data) {
-          angular.forEach(data, function(value, key) {
-
-            if (value.id == id) {
-              // value.id = id;
-              // value.title= $scope.commentData.title;
-              // value.body= $scope.commentData.body;
-              // value.author = $scope.commentData.author;
-              // value.date = $scope.commentData.date;
-              $scope.commentData.id = id;
-              $scope.commentData.title = value.title;
-              $scope.commentData.body = value.body;
-              $scope.commentData.author = value.author;
-              $scope.commentData.date = value.date;
-              console.log($scope.commentData);
-              // console.log(value);
-              // comment.save()
-          }
-
-      });
-          $scope.comments = data;
-          $scope.loading = false;
-      });
-
-
-
-    }
-    $scope.updateComment = function(id) {
+              })
+              .error(function(data) {
+                  console.log(data);
+              });
+      };
+      $scope.updateSubmitForm = function(id) {
         //Load the comments
         $scope.loading = true;
         //Get the comment based on ID
         Comment.get()
           .success(function(data) {
+            //go over each value, check if the item already exists
             angular.forEach(data, function(value, key) {
+              //if the id exists, update the scope
               if (value.id == id) {
-                deleteComment(id);
-              } else {
-                value.id = id - 1;
+                $scope.commentData.id = id;
+                $scope.commentData.title = value.title;
+                $scope.commentData.body = value.body;
+                $scope.commentData.author = value.author;
+                $scope.commentData.date = value.date;
+                console.log($scope.commentData);
+                // comment.save()
               }
-               submitComment();
-        });
+
+            });
+            // Reload the data
             $scope.comments = data;
             $scope.loading = false;
-        });
+          });
+      }
+      $scope.updateComment = function(id) {
+          //Load the comments
+          $scope.loading = true;
+          //Get the comment based on ID
+          Comment.get()
+            .success(function(data) {
+              angular.forEach(data, function(value, key) {
+                if (value.id == id) {
+                  deleteComment(id);
+                } else {
+                  value.id = id - 1;
+                }
+                 submitComment();
+          });
+              $scope.comments = data;
+              $scope.loading = false;
+          });
 
-    }
+      }
+      // function to handle deleting a comment
+      $scope.deleteComment = function(id) {
+          if (confirm("Are you sure you want to delete this post?")) {
+          $scope.loading = true;
+          Comment.destroy(id)
+              .success(function(data) {
+                  // if successful, we'll need to refresh the comment list
+                  Comment.get()
+                      .success(function(getData) {
+                          $scope.comments = getData;
+                          $scope.loading = false;
+                     });
 
-        // function to handle deleting a comment
-        $scope.deleteComment = function(id) {
-            if (confirm("Are you sure you want to delete this post?")) {
-        // todo code for deletion
+              });
+          }
+      };
 
-            $scope.loading = true;
-
-            Comment.destroy(id)
-                .success(function(data) {
-
-                    // if successful, we'll need to refresh the comment list
-                    Comment.get()
-                        .success(function(getData) {
-                            $scope.comments = getData;
-                            $scope.loading = false;
-                       });
-
-                });
-            }
-        };
-
-    });
+});
--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,9 @@ Route::get('/', function()
 {
     return View::make('index');
 });
-Route::put('comment/{comment}', 'CommentController@update');
+
+
+//Route::put('comment/{comment}', 'CommentController@update');
 
 Route::group(['middleware' => ['web']], function () {
     Route::resource('comments', 'CommentController');
@@ -36,26 +38,3 @@ Route::group(array('prefix'=>'/templates/'),function(){
         return View::make('templates.'.$template);
     }));
 });
-
-
-// =============================================
-// API ROUTES ==================================
-// =============================================
-// Route::group(array(), function() {
-
-//     // since we will be using this just for CRUD, we won't need create and edit
-//     // Angular will handle both of those forms
-//     // this ensures that a user can't access api/create or api/edit when there's nothing there
-//     Route::resource('comments', 'CommentController', 
-//         array('except' => array('create', 'edit', 'update', 'delete')));
-// });
-
-
-// =============================================
-// CATCH ALL ROUTE =============================
-// =============================================
-// all routes that are not home or api will be redirected to the frontend
-// this allows angular to route them
-// Route::get('{any?}', function ($any = null) {
-//     return View::make('index');
-// })->where('any', '.*');
